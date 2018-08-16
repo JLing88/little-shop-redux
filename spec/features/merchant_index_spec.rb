fb113f70d08ff49f82341901ade2cc6451f4fa07
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-16 16:00:59 -0600

    Cleans up testing

--- spec/features/merchant_index_spec.rb
+++ spec/features/merchant_index_spec.rb
@@ -3,10 +3,9 @@ RSpec.describe 'merchant index page' do
     it 'should show all merchants' do
       merchant_1 = Merchant.create(name: 'Turing School')
       merchant_2 = Merchant.create(name: 'Google')
-      
+
       visit '/merchants'
 
-      save_and_open_page
       expect(page).to have_content(merchant_1.name)
       expect(page).to have_content(merchant_2.name)
     end
