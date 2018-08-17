567b7096eaddc08bbc29cba840872cfdb9e82f88
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-08-16 21:46:23 -0600

    Refactors validates_presence_of statement

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,3 @@
 class Item < ActiveRecord::Base
-  validates_presence_of :title
-  validates_presence_of :description
-  validates_presence_of :price
+  validates_presence_of :title, :description, :price
 end
