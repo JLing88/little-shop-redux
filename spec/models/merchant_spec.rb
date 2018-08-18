3e66566927399b9a2f1e515ff91e554d9c017a20
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-18 17:29:16 -0600

    Adds relationship to Merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,4 +2,9 @@ RSpec.describe Merchant, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:name) }
   end
+
+  it 'should have many items' do
+    association = described_class.reflect_on_association(:items)
+    expect(association.macro).to eq :has_many
+  end
 end
