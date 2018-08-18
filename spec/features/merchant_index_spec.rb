752c6e4168227cb0fdf67f09b2cbdd4c12a80f6a
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-18 14:27:35 -0600

    Adds links from index to show for merchant

--- spec/features/merchant_index_spec.rb
+++ spec/features/merchant_index_spec.rb
@@ -2,12 +2,22 @@ RSpec.describe 'Merchant index page' do
   context 'As a visitor' do
     it 'should show all merchants' do
       merchant_1 = Merchant.create(name: 'Turing School')
-      merchant_2 = Merchant.create(name: 'Goo gle')
+      merchant_2 = Merchant.create(name: 'Google')
 
       visit '/merchants'
 
       expect(page).to have_content(merchant_1.name)
       expect(page).to have_content(merchant_2.name)
+    end
+
+    it 'should link to one merchants page' do
+      merchant_1 = Merchant.create(name: 'Turing School')
+      merchant_2 = Merchant.create(name: 'Google')
+
+      visit '/merchants'
+      click_on("#{merchant_1.name}")
+      expect(current_path).to eq("/merchants/#{merchant_1.id}")
+      expect(page).to have_content("#{merchant_1.name}")
     end
   end
 
