f9d660478e40e082c74a4b8a7ac62aca2eb3a7d0
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-08-18 14:49:36 -0600

    Adds delete functionality and tests

--- spec/features/item_show_spec.rb
+++ spec/features/item_show_spec.rb
@@ -1,27 +1,57 @@
 RSpec.describe 'Item show page' do
-  it 'should show one item' do
-    item_1 = Item.create(title: "Widget_1",
+  context 'Page should display' do
+    it 'one item' do
+      item_1 = Item.create(title: "Widget_1",
+                           description: "stuff",
+                           price: 299)
+
+      item_2 = Item.create(title: "Widget_2",
+                           description: "things",
+                           price: 1099)
+
+      visit '/items/1'
+
+      expect(page).to have_content(item_1.title)
+      expect(page).to_not have_content(item_2.title)
+    end
+
+    it 'an edit button' do
+      item = Item.create(title: "Widget_1",
                          description: "stuff",
                          price: 299)
 
-    item_2 = Item.create(title: "Widget_2",
-                         description: "things",
-                         price: 1099)
+      visit "/items/#{item.id}"
+      click_on('Edit')
 
-    visit '/items/1'
+      expect(current_path).to eq("/items/#{item.id}/edit")
+    end
 
-    expect(page).to have_content(item_1.title)
-    expect(page).to_not have_content(item_2.title)
+    it 'a delete button' do
+      item = Item.create(title: "Widget_1",
+                         description: "stuff",
+                         price: 299)
+
+      visit "/items/#{item.id}"
+      expect(page).to have_content("Delete")
+    end
   end
 
-  it 'should have an edit button' do
-    item = Item.create(title: "Widget_1",
-                       description: "stuff",
-                       price: 299)
+  context 'User is able to' do
+    it 'delete an item' do
+      item_1 = Item.create(title: "Widget_1",
+                           description: "stuff",
+                           price: 299)
 
-    visit "/items/#{item.id}"
-    click_on('Edit')
+      item_2 = Item.create(title: "Widget_2",
+                           description: "things",
+                           price: 1099)
 
-    expect(current_path).to eq("/items/#{item.id}/edit")
+      visit '/items'
+      click_on('Widget_1')
+      expect(current_path).to eq("/items/#{item_1.id}")
+      click_on('Delete')
+
+      expect(page).to_not have_content("Widget_1")
+    end
   end
 end
