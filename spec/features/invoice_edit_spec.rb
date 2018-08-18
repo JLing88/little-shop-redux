625b5002f4cac4919bfab174e545ebd3996efe4f
Harper Bellows <harper.bellows@gmail.com>
2018-08-18 14:53:55 -0600

    work continues on edit invoice

---
+++ spec/features/invoice_edit_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe Invoice do
+  describe 'Features' do
+    context 'when user is visiting /invoices' do
+      it 'can edit an invoice' do
+
+      merchant = Merchant.create(name: 'Joanne')
+      invoice = Invoice.create(merchant_id: 1, status: 'pending')
+
+      visit "/invoices/#{invoice.id}/edit"
+
+      find('.invoice-status-select').find(:option, "shipped").select_option
+
+
+        click_on('Update Invoice')
+        # save_and_open_page
+
+        expect(current_path).to eq("/invoices/#{invoice.id}")
+
+        expect(page).to have_content('shipped')
+      end
+    end
+  end
+end
