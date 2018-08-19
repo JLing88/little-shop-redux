8081a8ecf75045099653b02d2f18e07626acb9b5
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-19 13:59:13 -0600

    Adds seed for invoiceitem

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,3 +36,15 @@ CSV.foreach("./data/invoices.csv", headers: true, header_converters: :symbol) do
                 updated_at:  row[:updated_at]
                 )
 end
+
+CSV.foreach("./data/invoice_items.csv", headers: true, header_converters: :symbol) do |row|
+  InvoiceItem.create(
+                    id: row[:id],
+                    item_id: row[:item_id],
+                    invoice_id: row[:invoice_id],
+                    quantity: row[:quantity],
+                    unit_price: row[:unit_price],
+                    created_at:  row[:created_at],
+                    updated_at:  row[:updated_at]
+                )
+end
