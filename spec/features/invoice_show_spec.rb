75659df3b7ccad2892f1d6dc3ad774b9231961ed
Harper Bellows <harper.bellows@gmail.com>
2018-08-18 18:36:27 -0600

    wip destroy invoice

--- spec/features/invoice_show_spec.rb
+++ spec/features/invoice_show_spec.rb
@@ -1,8 +1,8 @@
-RSpec.describe 'invoice show page' do
+RSpec.describe 'Invoice show page' do
   context 'As a user' do
     it 'should show one invoice' do
       invoice = Invoice.create(merchant_id: 100, status: 'pending')
-      visit '/invoices/1'
+      visit "/invoices/#{invoice.id}"
 
       expect(page).to have_content(invoice.status)
     end
@@ -16,6 +16,17 @@ RSpec.describe 'invoice show page' do
         find_button('Edit').click
 
       expect(current_path).to eq("/invoices/#{invoice.id}/edit")
+    end
+    it 'should be able to delete invoice' do
+      merchant = Merchant.create(name: "Joe")
+      invoice = Invoice.create(merchant_id: 200, status: 'shipped')
+
+      visit "/invoices"
+
+      find_button('Delete').click
+
+
+      expect(page).to_not have_content("#{invoice.status}")
     end
   end
 end
