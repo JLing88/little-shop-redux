de3d5fc2f61ca833bd75697c1438fcf1104632c9
Harper Bellows <harper.bellows@gmail.com>
2018-08-18 21:16:57 -0600

    add delete from show and index

--- spec/features/invoice_index_spec.rb
+++ spec/features/invoice_index_spec.rb
@@ -6,8 +6,18 @@ RSpec.describe 'invoice index page' do
 
         visit '/invoices'
 
-        expect(page).to have_content(invoice_1.status)
-        expect(page).to have_content(invoice_2.status)
+        expect(page).to have_content(invoice_1.id)
+        expect(page).to have_content(invoice_2.id)
+    end
+    it 'user can delete an invoice from index page' do
+      invoice_2 = Invoice.create(merchant_id: 2, status: 'complete')
+
+      visit '/invoices'
+      save_and_open_page
+
+        click_on('Delete')
+
+      expect(page).not_to have_content('status: complete')
     end
   end
 end
--- spec/features/invoice_show_spec.rb
+++ spec/features/invoice_show_spec.rb
@@ -17,16 +17,16 @@ RSpec.describe 'Invoice show page' do
 
       expect(current_path).to eq("/invoices/#{invoice.id}/edit")
     end
-    it 'should be able to delete invoice' do
-      merchant = Merchant.create(name: "Joe")
-      invoice = Invoice.create(merchant_id: 200, status: 'shipped')
+    it 'user can delete an invoice from show page' do
+      invoice_1 = Invoice.create(merchant_id: 5, status: 'pending')
+      invoice_2 = Invoice.create(merchant_id: 2, status: 'complete')
 
-      visit "/invoices"
-
-      find_button('Delete').click
-
-
-      expect(page).to_not have_content("#{invoice.status}")
+      visit '/invoices'
+      click_on(invoice_1.id)
+      expect(current_path).to eq("/invoices/#{invoice_1.id}")
+      click_on('Delete')
+      expect(current_path).to eq('/invoices')
+      expect(page).not_to have_content('1')
     end
   end
 end
