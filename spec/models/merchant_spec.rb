a912292859097b09739d64be58487f640b482540
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-20 17:13:43 -0600

    Adds relationships tests for merchants and invoiceitems

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,8 +3,7 @@ RSpec.describe Merchant, type: :model do
     it { should validate_presence_of(:name) }
   end
 
-  it 'should have many items' do
-    association = described_class.reflect_on_association(:items)
-    expect(association.macro).to eq :has_many
+  describe 'Relationships' do
+    it { should belong_to(:merchant) }
   end
 end
