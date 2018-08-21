f212040d897ac50a534cd576ad483a75d4c9521b
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-20 21:50:11 -0600

    Adds merchant's item's average price

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,7 +3,7 @@ class Merchant < ActiveRecord::Base
   has_many :invoices
   validates_presence_of :name
 
-  # def average_price
-  #   average(:)
-  # end
+  def average_price
+    items.average(:unit_price).round(2)
+  end
 end
