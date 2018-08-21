0b86f1b0de98d4cc67fdee8b8734dc0b657ba7aa
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-08-21 13:24:31 -0600

    Adds tests and implements showing items on invoice show page

--- spec/features/invoice_show_spec.rb
+++ spec/features/invoice_show_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Invoice show page' do
 
       expect(page).to have_content(invoice.status)
     end
-      it 'sends user to edit page' do
+    it 'sends user to edit page' do
       merchant = Merchant.create(name: "Joe")
       invoice = merchant.invoices.create(status: 'shipped')
 
@@ -18,6 +18,35 @@ RSpec.describe 'Invoice show page' do
 
       expect(current_path).to eq("/invoices/#{invoice.id}/edit")
     end
+    it 'shows list of all associated item titles, quantities, unit_prices' do
+      item_1 = Item.create(title: "Widget_1",
+                           description: "stuff",
+                           unit_price: 299)
+
+      item_2 = Item.create(title: "Widget_2",
+                           description: "things",
+                           unit_price: 1099)
+
+      invoice = Invoice.create(customer_id: 1, status: 'shipped', merchant_id: 1)
+
+      ii_1 = invoice.invoice_items.create(item_id: item_1.id, unit_price: 100, quantity: 2)
+      ii_2 = invoice.invoice_items.create(item_id: item_2.id, unit_price: 50, quantity: 3)
+
+      ii_1_price = "$1.00"
+      ii_2_price = "$0.50"
+
+      visit "/invoices/#{invoice.id}"
+
+      expect(page).to have_content(item_1.id)
+      expect(page).to have_content(item_2.id)
+      expect(page).to have_content("#{item_1.title}")
+      expect(page).to have_content("#{item_2.title}")
+      expect(page).to have_content("#{ii_1.quantity}")
+      expect(page).to have_content("#{ii_2.quantity}")
+      expect(page).to have_content(ii_1_price)
+      expect(page).to have_content(ii_2_price)
+    end
+
     it 'user can delete an invoice from show page' do
       merchant_1 = Merchant.create(name: "Joe")
       merchant_2 = Merchant.create(name: "Joelle")
