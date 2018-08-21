b55b0c11f114362f953dec1ffa5190234cd453ce
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-08-21 17:03:31 -0600

    Adds link to newest item and tests

--- spec/features/item_dashboard_spec.rb
+++ spec/features/item_dashboard_spec.rb
@@ -71,4 +71,46 @@ RSpec.describe "Item Dashboard" do
 
     expect(page).to have_content("Oldest item: #{item_1.title}")
   end
+
+  it 'should have a link that redirects to oldest item' do
+    item_1 = Item.create(title: "Widget_1",
+                         description: "stuff",
+                         unit_price: 299)
+
+    item_2 = Item.create(title: "Widget_2",
+                         description: "more stuff",
+                         unit_price: 1199)
+
+    item_3 = Item.create(title: "Widget_3",
+                         description: "even more stuff",
+                         unit_price: 1000)
+
+    visit '/items-dashboard'
+    save_and_open_page
+
+    click_on('oldest-item')
+
+    expect(current_path).to eq("/items/#{item_1.id}")
+  end
+
+  it 'should have a link that redirects to oldest item' do
+    item_1 = Item.create(title: "Widget_1",
+                         description: "stuff",
+                         unit_price: 299)
+
+    item_2 = Item.create(title: "Widget_2",
+                         description: "more stuff",
+                         unit_price: 1199)
+
+    item_3 = Item.create(title: "Widget_3",
+                         description: "even more stuff",
+                         unit_price: 1000)
+
+    visit '/items-dashboard'
+    save_and_open_page
+
+    click_on('newest-item')
+
+    expect(current_path).to eq("/items/#{item_3.id}")
+  end
 end
